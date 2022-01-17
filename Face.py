cea93c07abc22f60e945c1eedf5692b96dc95968
attakay78 <richardquaicoe78@gmail.com>
2022-01-17 12:25:47 +0000

    added card faces as enum in Face.py

---
+++ Face.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+class Face(Enum):
+    TWO = 2.0
+    THREE = 3.0
+    FOUR = 4.0
+    FIVE = 5.0
+    SIX = 6.0
+    SEVEN = 7.0
+    EIGHT = 8.0
+    NINE = 9.0
+    TEN = 10.0
+    JACK = 10.1
+    QUEEN = 10.2
+    KING = 10.3
+    ACE = 11
