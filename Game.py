25bc75ee722c62269676ebb09c750d819af0030a
attakay78 <richardquaicoe78@gmail.com>
2022-01-26 15:42:49 +0000

    made changes to demo.py file

--- Game.py
+++ Game.py
@@ -9,6 +9,8 @@ from Face import Face
 from random import shuffle
 
 class Game:
+
+    # Constructor with some instance variables
     def __init__(self, args) -> None:
         self.list_of_players = []
         self.list_of_names = args
@@ -19,57 +21,69 @@ class Game:
         self.game_status = None
 
 
-    def createDeck(self) -> list:
+    # Function to create deck of cards from Suit and Face enum constants
+    def create_deck(self) -> list:
         return [(int(face.value), suit.name) for suit in Suit for face in Face]
 
 
-    def shuffleDeck(self) -> list:
-        new_deck : list = self.createDeck()
+    # Function to shuffle deck of cards using shuffle method
+    # from random module
+    def shuffle_deck(self) -> list:
+        new_deck : list = self.create_deck()
         shuffle(new_deck)
         return new_deck
 
-
-    def createPlayers(self) -> None :
+    # Function to create players from names in list_of_names variable
+    def create_players(self) -> None :
         for name in self.list_of_names:
             self.list_of_players.append(Player(name))
 
 
-    def getAllPlayers(self) -> list:
+    # Players getter function
+    def get_all_players(self) -> list:
         return self.list_of_players
 
 
-    def dealCardsToPlayers(self) -> None:
+    # Function to deal cards to players
+    def deal_cards_to_players(self) -> None:
         for player in self.list_of_players:
             card1 : tuple = self.shuffled_deck.pop(0)
             card2 : tuple = self.shuffled_deck.pop(0)
-            player.addCard(card1)
-            player.addCard(card2)
+            player.add_card(card1)
+            player.add_card(card2)
 
 
-    def setPlayerStatus(self, player : Player) -> None:
-        score = player.getScore()
+    # Function to set player status based on current score
+    def set_player_status(self, player : Player) -> None:
+        # getting player score
+        score = player.get_score()
         if(score < 17):
-            player.setStatus(PlayerStatus.HIT)
+            player.set_status(PlayerStatus.HIT)
         elif(score >= 17 and score <= 21):
-            player.setStatus(PlayerStatus.STICK)
+            player.set_status(PlayerStatus.STICK)
         else:
-            player.setStatus(PlayerStatus.BUST)
+            player.set_status(PlayerStatus.BUST)
             
 
+    # Function to start game
     def start(self) -> None :
+        # Set game status to START
         self.game_status = GameStatus.START
 
-        self.createDeck()
-        self.shuffled_deck = self.shuffleDeck()
-        self.createPlayers()
+        self.create_deck()
+        self.shuffled_deck = self.shuffle_deck()
+        self.create_players()
 
+        # Set game status to IN_MOTION
         self.game_status = GameStatus.IN_MOTION
 
-        self.dealCardsToPlayers()
+        self.deal_cards_to_players()
         
+        # Looping over list of players and adding them to 
+        # a list based on their status
         for player in self.list_of_players:
-            self.setPlayerStatus(player)
-            status = player.getStatus()
+            self.set_player_status(player)
+            status = player.get_status()
 
             if(status == PlayerStatus.HIT):
                 self.players_hit.append(player)
@@ -79,63 +93,79 @@ class Game:
                 self.players_bust.append(player)
 
 
+        # Loop for game to continue until game status changes to FINISH
         while(self.game_status != GameStatus.FINISH):
             
+            # Checking for players with score equal 21 after dealing 
+            # cards to players the first time
             scored_21 = []
             for player in self.players_stick:
-                score = player.getScore()
+                score = player.get_score()
                 if(score == 21):
                     scored_21.append(player)
 
             if(len(scored_21) == 1):
-                print(f"Game ended, won by {scored_21[0].getName()} with score {scored_21[0].getScore()}")
+                print(f"Game ended, won by {scored_21[0].get_name()} with score {scored_21[0].get_score()}")
                 self.game_status = GameStatus.FINISH
             elif(len(scored_21) > 1):
                 print("There is a tie")
                 self.game_status = GameStatus.FINISH
             else:
+                # Checking if all players got busted
                 if(len(self.players_bust) == len(self.list_of_players)):
                     self.game_status = GameStatus.FINISH
                     print("No winner, All players busted!!!")
 
+                # Checking if any player exist in players_hit list. Deal
+                # cards to players until no player left in players_hit list
                 elif(len(self.players_hit) > 0):
+
+                    # Assign players_hit to new_players_hit to
+                    # prevent concurrentModification error
                     new_players_hit : list = self.players_hit
 
                     while(len(new_players_hit) != 0):
                         for player in self.players_hit:
                             card : tuple = self.shuffled_deck.pop(0)
-                            player.addCard(card)
+                            player.add_card(card)
 
-                            self.setPlayerStatus(player)
+                            self.set_player_status(player)
 
-                            status = player.getStatus()
+                            status = player.get_status()
                             
                             if(status == PlayerStatus.STICK):
+                                # Remove players with new status STICK from players_hit
+                                # to players_stick list
                                 new_players_hit.remove(player)
                                 self.players_stick.append(player)
                             elif(status == PlayerStatus.BUST):
+                                # Remove players with new status BUST from players_hit
+                                # to players_bust list
                                 new_players_hit.remove(player)
                                 self.players_bust.append(player)
                         
                         self.players_hit = new_players_hit
                 else:
+                    # Logic to check for game winner based on players
+                    # in players_stick list with highest score or 
+                    # players with same highest score
                     multi_winner : list = []
                     winner : Player = None
                     score : int = 0
                     for player in self.players_stick:
-                        player_score = player.getScore()
+                        player_score = player.get_score()
                         if(player_score == score):
                             multi_winner.append(player)
-                        if(player.getScore() > score):
+                        if(player.get_score() > score):
                             multi_winner = []
                             multi_winner.append(player)
-                            score = player.getScore()
+                            score = player.get_score()
                             winner = player
 
                     if(len(multi_winner) > 1):
                         print("There is a tie")
                     else:    
-                        print(f"Game ended, won by {winner.getName()} with score {winner.getScore()}")
+                        print(f"Game ended, won by {winner.get_name()} with score {winner.get_score()}")
 
                     self.game_status = GameStatus.FINISH
                 
@@ -144,6 +174,7 @@ class Game:
         
 
 
-game = Game(argv[1:])
-game.start()
+if __name__ == "__main__":
+    game = Game(argv[1:])
+    game.start()
 
